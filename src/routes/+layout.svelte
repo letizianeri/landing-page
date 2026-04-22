807845bb2be9541ec6afb884558ea11b4979be53
letizianeri <letizia.neri@mail.polimi.it>
2026-04-22 20:57:19 +0200

    import files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+	import favicon from "$lib/assets/favicon.svg";
+
+	import "$lib/styles/reset.css";
+	import "$lib/styles/tokens.css";
+	import "$lib/styles/base.css";
+	import "$lib/styles/utilities.css";
+
+	import Header from "$lib/components/Header.svelte";
+	import Footer from "$lib/components/Footer.svelte";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+
+	<!-- Satoshi Font -->
+	<link rel="preconnect" href="https://api.fontshare.com/" />
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet" />
+
+	<!-- Inter Font -->
+	<link rel="preconnect" href="https://rsms.me/" />
+	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+</svelte:head>
+
+<Header />
+
+{@render children()}
+
+<Footer />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	import Filter from "$lib/components/Filter.svelte";
+	import Project from "$lib/components/Project.svelte";
+
+	const { data } = $props();
+
+	let currentYear = $state(data.years[0].number);
+	let projects = $derived.by(() => {
+		return data.years.find((year) => {
+			return year.number == currentYear;
+		}).projects;
+	});
+</script>
+
+<section class="safe-area hero">
+	<h1>
+		Il MEI - Meeting Etichette Indipendenti è una manifestazione musicale che raduna 
+		le principali produzioni discografiche indipendenti ed emergenti italiane a Faenza.
+		L’evento si svolge durante la prima settimana di ottobre, tutti gli anni, dal 1995.
+	</h1>
+</section>
+
+<nav class="safe-area filters">
+	{#each data.years as year}
+		<Filter bind:group={currentYear} value={year.number} />
+	{/each}
+</nav>
+
+<section class="safe-area projects">
+	{#each projects as project}
+		<Project data={project.data} />
+	{/each}
+</section>
+
+<style>
+	.hero {
+		padding-block: var(--size-11);
+
+		h1 {
+			font-size: var(--size-7);
+			max-width: 35ch;
+		}
+	}
+
+	.filters {
+		display: flex;
+		align-items: center;
+		gap: var(--size-5);
+	}
+
+	.projects {
+		padding-block: var(--size-7);
+
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: var(--size-5);
+	}
+</style>
